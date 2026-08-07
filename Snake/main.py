4fc70cbfa21521a9db5e874c0845c2a843e467a0
Jose Laurito <jigolaurito93@yahoo.com>
2026-08-06 22:01:28 -0400

    create scoreboard functionality

--- Snake/main.py
+++ Snake/main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 import time
 from snake import Snake
 from food import Food
+from scoreboard import Scorebored
 
 screen = Screen()
 screen.setup(width=668, height=688)
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scorebored()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -28,6 +30,7 @@ while game_is_on:
 
     # Detect the collision with food
     if snake.head.distance(food) < 15:
+        scoreboard.add_point()
         food.refresh()
 
 
---
+++ Snake/scoreboard.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 18, "normal")
+
+class Scorebored(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=(FONT))
+        self.hideturtle()
+    
+    def add_point(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=(FONT))
