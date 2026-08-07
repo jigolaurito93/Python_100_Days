4fd84f5ca58c20d4675bf5ee2bf42cb0ea26f6f5
Jose Laurito <jigolaurito93@yahoo.com>
2026-08-06 20:32:33 -0400

    snake class with controls

---
+++ Snake/main.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=668, height=688)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+# screen.update()
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(.1)
+    snake.move()
+
+
+# def move_up():
+#     new_segment.setheading(90)
+
+# def move_down():
+#     new_segment.setheading(270)
+# def move_left():
+#     new_segment.setheading(180)
+# def move_right():
+#     new_segment.setheading(0)
+
+# screen.listen()
+# screen.onkey(key="w", fun=move_up)
+# screen.onkey(key="s", fun=move_down)
+# screen.onkey(key="a", fun=move_left)
+# screen.onkey(key="d", fun=move_right)
+
+screen.exitonclick()
