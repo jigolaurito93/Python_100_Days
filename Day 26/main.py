1e85db21b2c532d756eee0f5e006637ace42a40f
Jose Laurito <jigolaurito93@yahoo.com>
2026-08-14 22:39:13 -0400

    dictionary comprehension

--- Day 26/main.py
+++ Day 26/main.py
@@ -1,3 +1,5 @@
+import random
+
 # numbers = [1, 2, 3]
 # new_numbers = [n + 1 for n in numbers]
 # print(new_numbers)
@@ -6,7 +8,11 @@
 # print(doubled_nums)
 
 names = ["Alex", "Beth", "Charlie", "Daniel", "Elizabeth", "Fran", "Grof"]
-# short_names = [name for name in names if len(name) <= 4]
-# print(short_names)
-long_names = [name.upper() for name in names if len(name) >= 5]
-print(long_names)
+# # short_names = [name for name in names if len(name) <= 4]
+# # print(short_names)
+# long_names = [name.upper() for name in names if len(name) >= 5]
+# print(long_names)
+student_scores = {student:random.randint(1,100) for student in names}
+passed_students = {key:value for (key, value) in student_scores.items() if value >= 75}
+print(student_scores)
+print(passed_students)
