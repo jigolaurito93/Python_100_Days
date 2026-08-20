5662b5a8baf10fb5bd3f55b091ea1754dc1d5b2d
Jose Laurito <jigolaurito93@yahoo.com>
2026-08-20 03:03:55 -0400

    improve error handling for nato alphabet app

--- NATO Alphabet/main.py
+++ NATO Alphabet/main.py
@@ -4,8 +4,16 @@ alphabet = pandas.read_csv("NATO Alphabet/nato_phonetic_alphabet.csv")
 
 nato_dict = {value.letter:value.code for (row, value) in  alphabet.iterrows()}
 
-given_word = input("Enter a word: ").upper()
+while True:
 
-
-converted_letters = [nato_dict[letter] if letter in nato_dict else letter for letter in given_word]
-print(converted_letters)
+    given_word = input("Enter a word: ").upper()
+    
+    # converted_letters = [nato_dict[letter] if letter in nato_dict else letter for letter in given_word]
+    
+    try:
+        converted_letters = [nato_dict[letter] for letter in given_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(converted_letters)
+        break
